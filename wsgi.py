e52a7e788cd44b234c285be76efc3a622e1a724e
Sriram Guddati <sriramguddati647@gmail.com>
2026-04-30 14:43:58 +0530

    Refactor WSGI config and add PORT environment variable

--- wsgi.py
+++ wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stroke.settings')
 
 application = get_wsgi_application()
+import os
+PORT = os.environ.get("PORT", 8000)
